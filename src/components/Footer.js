3e261f33d03caa3b74451f22fbe6eca306694bb2
Krum Kuzmov <kkuzmov@gmail.com>
2021-03-16 17:16:34 +0200

    add footer

---
+++ src/components/Footer.js
@@ -0,0 +1,11 @@
+import { Component } from "react";
+
+class Footer extends Component{
+    render(){
+        return(
+            <footer class="main-footer">&copy; 2021 by Krum Kuzmov for SoftUni</footer>
+        )
+    }
+}
+
+export default Footer;
