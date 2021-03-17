5e44b8bc5bc6c88ebb9a555cefb4658d7c45996a
Krum Kuzmov <kkuzmov@gmail.com>
2021-03-17 18:03:59 +0200

    add test database

---
+++ src/components/Database/Database.js
@@ -0,0 +1,21 @@
+let database = [
+    {
+        "id": 1,
+        "name": 'Simeonovo Restaurant',
+        "description": 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Totam, autem.',
+        "imageUrl": 'https://www.mychoice.bg/wp-content/uploads/2020/05/959x578-restorant-1-1.jpg',
+    },
+    {
+        "id": 2,
+        "name": 'Boyana Restaurant',
+        "description": 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Totam, autem.',
+        "imageUrl": 'https://socialcafe.bg/pics/restaurant/2.jpg',
+    },
+    {
+        "id": 3,
+        "name": 'Bankya Restaurant',
+        "description": 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Totam, autem.',
+        "imageUrl": 'https://rezzo.bg/files/images/14/fit_314_228.jpg',
+    },
+
+]
