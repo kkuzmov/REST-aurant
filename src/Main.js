dfd0b54d5c8769543a67ac7f93ae619b7e5a06c9
Krum Kuzmov <kkuzmov@gmail.com>
2021-03-17 17:06:17 +0200

    add guest homepage

--- src/Main.js
+++ src/Main.js
@@ -1,16 +1,18 @@
 import React, {Component} from 'react';
 import Header from './components/Header';
 import Footer from './components/Footer';
+import HomepageGuest from './components/HomepageGuest';
 import { style } from './Main.css';
 
 
 class Main extends Component{
     render(){
         return(
-            <div>
+            <>
            <Header />
+           <HomepageGuest />
            <Footer />
-           </div>
+           </>
         )
     }
 }
---
+++ src/components/HomepageGuest.js
@@ -0,0 +1,39 @@
+import { Component } from 'react';
+import { style } from './HomepageGuest.css';
+
+
+class HomepageGuest extends Component{
+    constructor(props){
+        super(props)
+    }
+    render(){
+        return (
+    <main>
+        <h1 class="welcome">Welcome to REST-aurant!</h1>
+        <p class="lorem-ipsum">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis alias ullam repudiandae quos voluptatum debitis vero dicta error soluta perferendis et eveniet quidem, aliquid quas assumenda non? Eligendi, sit voluptatum.</p>
+        <article class="all-rated-restaurants">
+            <article class="restaurant-card card-left-guest">
+                <img src="https://www.mychoice.bg/wp-content/uploads/2020/05/959x578-restorant-1-1.jpg" alt="simeonovo-restaurant"/>
+                <h4>Simeonovo Restaurant</h4>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Totam, autem.</p>
+            </article>
+            <article class="restaurant-card card-middle-guest">
+                <img src="https://socialcafe.bg/pics/restaurant/2.jpg" alt="boyana-restaurant"/>
+                <h4>Boyana Restaurant</h4>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Totam, autem.</p>
+            </article>
+            <article class="restaurant-card card-right-guest">
+                <img src="https://rezzo.bg/files/images/14/fit_314_228.jpg" alt="bankya-restaurant"/>
+                <h4>Bankya Restaurant</h4>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Totam, autem.</p>
+            </article>       
+        </article>
+        <article class="login-or-signup">
+            <h2><a href="#">Login</a> or <a href="#">create a profile</a> to view all ratings</h2>
+        </article>
+    </main>
+        )
+    }
+}
+
+export default HomepageGuest;
