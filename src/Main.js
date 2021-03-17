e28e382ea5f74efb354220e1f9b05a6eec458559
Krum Kuzmov <kkuzmov@gmail.com>
2021-03-17 17:08:37 +0200

    move all components in separate dirs

--- src/Main.js
+++ src/Main.js
@@ -1,8 +1,10 @@
 import React, {Component} from 'react';
-import Header from './components/Header';
-import Footer from './components/Footer';
-import HomepageGuest from './components/HomepageGuest';
+import Header from './components/Header/Header';
+import Footer from './components/Footer/Footer';
+import HomepageGuest from './components/HomepageGuest/HomepageGuest';
 import { style } from './Main.css';
+
+//TESTING WITH ONE LOOK PER TIME - LEAVE ONLY HEADER AND FOOTER AS CONSTANTS!
 
 
 class Main extends Component{
