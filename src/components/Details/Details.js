b94803036fe106dcd0a263da8d52dc3fbec155b6
Krum Kuzmov <kkuzmov@gmail.com>
2021-03-20 13:18:18 +0200

    add route to real details!

--- src/components/Details/Details.js
+++ src/components/Details/Details.js
@@ -1,20 +1,23 @@
 import { Component } from "react";
 import { style } from './Details.css';
+import { Route, Link, NavLink, Switch, Redirect } from 'react-router-dom';
 
+import database from '../Database/Database';
 import map from './map.jpeg'
 
 
 class Details extends Component{
     render(){
+        let restaurant = database.find(x => x.id == this.props.match.params.id);
         return(
             <>
             <h1 className="details-heading">Restaurant details</h1>
             <article className="restaurant-details-cointainer">
-                <img src="https://www.mychoice.bg/wp-content/uploads/2020/05/959x578-restorant-1-1.jpg" alt="simeonovo-restaurant" />
-                <h1 className="restaurant-details-name">Simeonovo Restaurant</h1>
+                <img src={restaurant.imageUrl} alt={restaurant.name} />
+                <h1 className="restaurant-details-name">{restaurant.name}</h1>
                 <h3 className="rated-by">Rated by 40 people</h3>
-                <h3 className="rated-by">Average rating: 4.5 *</h3>
-                <p className="restaurant-details-description">Lorem ipsum dolor sit amet consectetur adipisicing elit. Maiores, iste maxime. Quasi error harum architecto repellendus deserunt quaerat quisquam eaque aperiam dolor magnam est impedit molestias, nesciunt optio eveniet, quibusdam odit! Voluptatibus commodi, iste non neque quos accusamus autem voluptatem maiores doloribus minus. Nobis vel, praesentium nam eveniet sequi modi.</p>
+                <h3 className="rated-by">Average rating: {restaurant.rating} *</h3>
+                <p className="restaurant-details-description">{restaurant.description}</p>
                 <p><img src={map} alt="map" className="google-api-sample-pic" /></p>
                 <article className="details-buttons">
                     <button className="site-button">Save</button>
