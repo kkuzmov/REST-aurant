03cb7b678341797d9f6352e6b16d7102b7e475a1
Krum Kuzmov <kkuzmov@gmail.com>
2021-03-20 13:07:17 +0200

    add route to details

--- src/components/Database/Database.js
+++ src/components/Database/Database.js
@@ -4,35 +4,40 @@ let database = [
         "name": 'Fish and chips Restaurant',
         "description": 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Totam, autem.',
         "imageUrl": 'https://www.mychoice.bg/wp-content/uploads/2020/05/959x578-restorant-1-1.jpg',
-        "location": "Simeonovo, Sofia"
+        "location": "Simeonovo, Sofia",
+        "rating": 4.4
     },
     {
         "id": 2,
         "name": 'Foies gras Restaurant',
         "description": 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Totam, autem.',
         "imageUrl": 'https://socialcafe.bg/pics/restaurant/2.jpg',
-        "location": "Boyana, Sofia"
+        "location": "Boyana, Sofia",
+        "rating": 4.6
     },
     {
         "id": 3,
         "name": 'Pancetta Restaurant',
         "description": 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Totam, autem.',
         "imageUrl": 'https://rezzo.bg/files/images/14/fit_314_228.jpg',
-        "location": "Bankya, Sofia"
+        "location": "Bankya, Sofia",
+        "rating": 4.1
     },
     {
         "id": 4,
         "name": 'Babatunde Restaurant',
         "description": 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Totam, autem.',
         "imageUrl": 'https://media-cdn.tripadvisor.com/media/photo-s/0f/28/61/86/restaurant-view.jpg',
-        "location": "Dragalevtsi, Sofia"
+        "location": "Dragalevtsi, Sofia",
+        "rating": 4.7
     },
     {
         "id": 5,
         "name": 'French rack Restaurant',
         "description": 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Totam, autem.',
         "imageUrl": 'https://www.visitsofia.bg/images/vegas_media/category20000/object956/373ae7985b4fc356a958decb6de28704.jpg',
-        "location": "Lozenets, Sofia"
+        "location": "Lozenets, Sofia",
+        "rating": 4.9
     },
 
 ]
--- src/components/Restaurant/Restaurant.js
+++ src/components/Restaurant/Restaurant.js
@@ -1,18 +1,22 @@
 import { Component } from 'react';
 import { style } from './Restaurant.css';
+import { Link } from 'react-router-dom';
+
  
 class Restaurant extends Component{
     constructor(props){
         super(props)
     }
     render(){
+        let url = `/details/${this.props.id}`
+
         return (
             <article className="restaurant">
                 <img src={this.props.imageUrl} alt={this.props.name} />
                 <p className="restaurant-name"><i className="fas fa-utensils"></i> {this.props.name}</p>
                 <p className="restaurant-city"><i className="fas fa-map-marker-alt"></i> {this.props.location}</p>
-                <p className="restaurant-rating"><i className="fas fa-smile"></i> 4.4</p>
-                <button className="restaurant-more-info site-button">Read more</button>
+                <p className="restaurant-rating"><i className="fas fa-smile"></i> {this.props.rating}</p>
+                <Link to={url}><button className="restaurant-more-info site-button">Read more</button></Link> 
             </article>
         )
     }
