d237bfa2af79d3e7637148f5f718f51936bbcb07
Krum Kuzmov <kkuzmov@gmail.com>
2021-03-21 16:48:22 +0200

    add 404 page

--- src/components/ErrorPage/ErrorPage.js
+++ src/components/ErrorPage/ErrorPage.js
@@ -1,5 +1,5 @@
 import { Component } from 'react';
-
+import { Link } from 'react-router-dom';
 
 class ErrorPage extends Component{
     constructor(props){
@@ -7,7 +7,11 @@ class ErrorPage extends Component{
     }
     render(){
         return(
+            <article className="error-container">
             <h1 className="page-heading">Nothing here!</h1>
+            <img src="https://cdn.rawgit.com/ahmedhosna95/upload/1731955f/sad404.svg" alt="error-page" className="error-image"></img>
+            <h3>Go <Link to="/">home</Link> now...</h3>
+            </article>
         )
     }
 }
