fa42c91551bf49bf0fb482dcef7e77d3242e11d2
Krum Kuzmov <kkuzmov@gmail.com>
2021-03-21 16:50:39 +0200

    fix error page - final

--- src/components/ErrorPage/ErrorPage.js
+++ src/components/ErrorPage/ErrorPage.js
@@ -8,9 +8,8 @@ class ErrorPage extends Component{
     render(){
         return(
             <article className="error-container">
-            <h1 className="page-heading">Nothing here!</h1>
+            <h1 className="page-heading">404 - Page not found!</h1>
             <img src="https://cdn.rawgit.com/ahmedhosna95/upload/1731955f/sad404.svg" alt="error-page" className="error-image"></img>
-            <h3>Go <Link to="/">home</Link> now...</h3>
             </article>
         )
     }
