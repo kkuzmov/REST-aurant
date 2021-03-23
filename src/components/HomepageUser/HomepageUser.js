aa3d9cbfd1af3d4e2c769ad07700e113e64c58f2
Krum Kuzmov <kkuzmov@gmail.com>
2021-03-23 19:04:37 +0200

    Fix homepage user with real DB

--- src/components/HomepageUser/HomepageUser.js
+++ src/components/HomepageUser/HomepageUser.js
@@ -1,36 +1,48 @@
-import { Component } from 'react';
 import { style } from './HomepageUser.css';
-import RestaurantHomepage from '../RestaurantHomepage/RestaurantHomepage';
-import database from '../Database/Database';
 
-class Header extends Component{
+import { Component } from 'react';
+import RestaurantHomepage from '../RestaurantHomepage/RestaurantHomepage';
+import db from '../../firebase/firebase.config';
+
+
+
+class HomepageUser extends Component{
     constructor(props){
         super(props)
 
         this.state = {
-            pets: []
+            'restaurants': []
         }
+        this.getRestaurants = this.getRestaurants.bind(this);
     }
 
+    getRestaurants(){
+        db.collection('restaurants')
+        .get()
+        .then(res =>{
+            let allFetched = []
+            res.docs.forEach(restaurant => {
+                allFetched.push({...restaurant.data(), id: restaurant.id})
+            })
+            this.setState({'restaurants': allFetched})
+        })
+    }
     componentDidMount(){
-        fetch('https://fisher-game.firebaseio.com/catches.json')
-            .then(res => res.json())
-            .then(response => this.setState({pets: response}))
+        this.getRestaurants();
     }
     render(){
-        let allRestaurants = database.map(x=> x = <RestaurantHomepage imageUrl={x.imageUrl} name={x.name} description={x.description} id={x.id}/>)
-        let allPets = Object.values(this.state.pets).map(x => <p>{x.location}</p>)
+        console.log(this.state.restaurants)
+        let allRestaurants = this.state.restaurants.map(x => x = <RestaurantHomepage key={x.id} imageUrl={x.imageUrl} name={x.name} description={x.description} id={x.id}/>) || 'Loading...'
         return (
             <main>
-                <article>{allPets}</article>
                 <h1 className="top-rated">Welcome, user!</h1>
                 <h1 className="top-rated">Check out our current top rated restaurants: </h1>
                 <article className="all-restaurants-homepage">
-                {allRestaurants}
+                    {allRestaurants}
                 </article>
             </main>
         )
     }
 }
 
-export default Header;
+export default HomepageUser;
