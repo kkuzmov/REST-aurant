eee601c73d0a7393cced43eed21bf52b6b079465
Krum Kuzmov <kkuzmov@gmail.com>
2021-03-23 19:52:07 +0200

    add details route with firebase DB

--- src/components/Details/Details.js
+++ src/components/Details/Details.js
@@ -1,23 +1,38 @@
 import { Component } from "react";
 import { style } from './Details.css';
-import { Route, Link, NavLink, Switch, Redirect } from 'react-router-dom';
-
 import database from '../Database/Database';
+import { Route, Link, NavLink, Switch, Redirect } from 'react-router-dom';
+import db from '../../firebase/firebase.config';
+
 import map from './map.jpeg'
 
 
 class Details extends Component{
+    constructor(props){
+        super(props)
+        this.state = {}
+        this.getOneRestaurant = this.getOneRestaurant.bind(this);
+    }
+    getOneRestaurant(id){
+        db.collection('restaurants')
+            .doc(id)
+            .get()
+            .then(res => this.setState({...res.data()}))
+    }
+    componentDidMount(){
+        this.getOneRestaurant(this.props.match.params.id);
+    }
     render(){
-        let restaurant = database.find(x => x.id == this.props.match.params.id);
+        console.log(this.state)
         return(
             <>
-            <h1 className="details-heading">{restaurant.name}</h1>
+            <h1 className="details-heading">{this.state.name}</h1>
             <article className="restaurant-details-cointainer">
-                <img src={restaurant.imageUrl} alt={restaurant.name} />
-                <h1 className="restaurant-details-name">{restaurant.location}</h1>
-                <h3 className="rated-by">Rated by {restaurant.ratedBy} people</h3>
-                <h3 className="rated-by">Average rating: {restaurant.rating} *</h3>
-                <p className="restaurant-details-description">{restaurant.description}</p>
+                <img src={this.state.imageUrl} alt={this.state.name} />
+                <h1 className="restaurant-details-name">{this.state.location}</h1>
+                <h3 className="rated-by">Rated by {this.state.ratedBy} people</h3>
+                <h3 className="rated-by">Average rating: {this.state.rating} *</h3>
+                <p className="restaurant-details-description">{this.state.description}</p>
                 <p><img src={map} alt="map" className="google-api-sample-pic" /></p>
                 <article className="details-buttons">
                     <button className="site-button">Save</button>
