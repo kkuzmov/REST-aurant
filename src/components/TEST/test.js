a28a765ac5c113e81246d6e4ba16a5c73672726d
Krum Kuzmov <kkuzmov@gmail.com>
2021-03-23 18:55:28 +0200

    add a getRestaurants func

--- src/components/TEST/test.js
+++ src/components/TEST/test.js
@@ -11,27 +11,32 @@ class Header extends Component{
         this.state = {
             'restaurants': []
         }
+        this.getRestaurants = this.getRestaurants.bind(this);
     }
 
-    componentDidMount(){
-        db.collection('restaurants').get()
-            .then(res =>{
-                let allFetched = []
-                res.docs.forEach(restaurant => {
-                    allFetched.push({...restaurant.data(), id: restaurant.id})
-                })
-                this.setState({'restaurants': allFetched})
+    getRestaurants(){
+        db.collection('restaurants')
+        .get()
+        .then(res =>{
+            let allFetched = []
+            res.docs.forEach(restaurant => {
+                allFetched.push({...restaurant.data(), id: restaurant.id})
             })
+            this.setState({'restaurants': allFetched})
+        })
+    }
+    componentDidMount(){
+        this.getRestaurants();
     }
     render(){
         console.log(this.state.restaurants)
-
+        let allRestaurants = this.state.restaurants.map(x => x = <RestaurantHomepage key={x.id} imageUrl={x.imageUrl} name={x.name} description={x.description} id={x.id}/>) || 'Loading...'
         return (
             <main>
                 <h1 className="top-rated">Welcome, user!</h1>
                 <h1 className="top-rated">Check out our current top rated restaurants: </h1>
                 <article className="all-restaurants-homepage">
-                    {this.state.restaurants.map(x => x = <RestaurantHomepage key={x.id} imageUrl={x.imageUrl} name={x.name} description={x.description} id={x.id}/>)}
+                    {allRestaurants}
                 </article>
             </main>
         )
