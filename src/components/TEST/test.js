e795a2344e1be181842642580c263cf0ccfa209c
Krum Kuzmov <kkuzmov@gmail.com>
2021-03-23 16:47:03 +0200

    fetch data and save in state

--- src/components/TEST/test.js
+++ src/components/TEST/test.js
@@ -1,28 +1,37 @@
 import { Component } from 'react';
 import RestaurantHomepage from '../RestaurantHomepage/RestaurantHomepage';
 import db from '../../firebase/firebase.config';
-import database from '../Database/Database';
+
 
 
 class Header extends Component{
     constructor(props){
         super(props)
 
-        // this.state = {
-            
-        // }
+        this.state = {
+            'restaurants': []
+        }
     }
 
     componentDidMount(){
-        
+        db.collection('restaurants').get()
+            .then(res =>{
+                let allFetched = []
+                res.docs.forEach(restaurant => {
+                    allFetched.push({...restaurant.data()})
+                })
+                this.setState({'restaurants': allFetched})
+            })
     }
     render(){
+        console.log(this.state.restaurants)
 
         return (
             <main>
                 <h1 className="top-rated">Welcome, user!</h1>
                 <h1 className="top-rated">Check out our current top rated restaurants: </h1>
                 <article className="all-restaurants-homepage">
+                    {this.state.restaurants.map(x => x = <RestaurantHomepage imageUrl={x.imageUrl} name={x.name} description={x.description} id={x.id}/>)}
                 </article>
             </main>
         )
