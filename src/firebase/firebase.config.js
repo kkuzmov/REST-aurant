23a5cc3e7112eeae944ae5ee9ed2eea03db815c7
Krum Kuzmov <kkuzmov@gmail.com>
2021-03-23 15:46:02 +0200

    install firebase and add config

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,15 @@
+import firebase from 'firebase';
+
+var firebaseConfig = {
+    apiKey: "AIzaSyAYnzuKqsJl_2y2EJE9fMiKlZhqdwtue7E",
+    authDomain: "rest-aurant-71507.firebaseapp.com",
+    projectId: "rest-aurant-71507",
+    storageBucket: "rest-aurant-71507.appspot.com",
+    messagingSenderId: "901461881317",
+    appId: "1:901461881317:web:886d704a3725a3657e7f0c"
+  };
+  // Initialize Firebase
+  const firebase = firebase.initializeApp(firebaseConfig);
+  const db = firebase.firestore();
+
+  export default db;
