80ce67ee1a35f66c63de184131e6a98d7145a4f7
Krum Kuzmov <kkuzmov@gmail.com>
2021-03-24 09:56:15 +0200

    add working like button in details

--- src/components/Details/Details.js
+++ src/components/Details/Details.js
@@ -22,21 +22,23 @@ class Details extends Component{
     componentDidMount(){
         this.getOneRestaurant(this.props.match.params.id);
     }
+    likeRestaurant(){
+        this.setState((prevState) => ({ ratedBy: prevState.ratedBy + 1 }))
+    }
     render(){
-        console.log(this.state)
         return(
             <>
             <h1 className="details-heading">{this.state.name}</h1>
             <article className="restaurant-details-cointainer">
                 <img src={this.state.imageUrl} alt={this.state.name} />
                 <h1 className="restaurant-details-name">{this.state.location}</h1>
-                <h3 className="rated-by">Rated by {this.state.ratedBy} people</h3>
+                <h3 className="rated-by">Liked by {this.state.ratedBy} people</h3>
                 <h3 className="rated-by">Average rating: {this.state.rating} *</h3>
                 <p className="restaurant-details-description">{this.state.description}</p>
                 <p><img src={map} alt="map" className="google-api-sample-pic" /></p>
                 <article className="details-buttons">
                     <button className="site-button">Save</button>
-                    <button className="site-button">Like</button>
+                    <button className="site-button" onClick={()=>{this.likeRestaurant()}}>Like</button>
                 </article>
             </article>
             </>
