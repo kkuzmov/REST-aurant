58fba982dbe3cbb1f6a1e2c4982666018eb53d3b
Krum Kuzmov <kkuzmov@gmail.com>
2021-03-26 10:58:34 +0200

    refactor navigation to functional component

--- src/components/Header/Navigation/NavigationUser.js
+++ src/components/Header/Navigation/NavigationUser.js
@@ -2,20 +2,15 @@ import { Component } from 'react';
 import { style } from './Navigation.css';
 import { Link } from 'react-router-dom';
 
-class NavigationUser extends Component{
-    constructor(props){
-        super(props)
-    }
-    render(){
-        return (
-            <ul className="navigation">
-                <Link to="/create" className="nav-link">Create-a-rate</Link>
-                <Link to="/all-restaurants" className="nav-link">Restaurants</Link>
-                <Link to="/profile" className="nav-link">Profile</Link>
-                <Link to="/logout" className="nav-link">Logout</Link>
-            </ul>
-        )
-    }
+function NavigationUser(){
+    return (
+        <ul className="navigation">
+            <Link to="/create" className="nav-link">Create-a-rate</Link>
+            <Link to="/all-restaurants" className="nav-link">Restaurants</Link>
+            <Link to="/profile" className="nav-link">Profile</Link>
+            <Link to="/logout" className="nav-link">Logout</Link>
+        </ul>
+    )
 }
 
 export default NavigationUser;
