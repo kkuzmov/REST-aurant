58207f1d085c39ac5221f4af1995169d06b6e9e1
Krum Kuzmov <kkuzmov@gmail.com>
2021-03-30 18:11:14 +0300

    add validation to create restaurant route

--- src/services/Helpers/helpers.js
+++ src/services/Helpers/helpers.js
@@ -4,9 +4,17 @@ function testInput(input){
     }else if(input.name.length > 25){
         return 'Restaurant name is too long.'
     }else if(input.location.length < 3){
-        return 'Location name is too short.'
+        return 'Location is too short.'
     }else if(input.name.length > 25){
-        return 'Location name is too long.'
+        return 'Location is too long.'
+    }else if(isNaN(input.rating) || input.rating < 0 || input.rating > 5){
+        return 'Please enter a rate between 0 and 5.'
+    }else if(input.description.length < 10){
+        return 'Description is too short, please provide more information on the restaurant.'
+    }else if(input.description.length > 20){
+        return 'Description is too long, please be briefer.'
+    }else if(!input.imageUrl.startsWith('http') || !input.imageUrl.startsWith('https')){
+        return 'Please provide a correct link to photo.'
     }
     else{
         return '';
