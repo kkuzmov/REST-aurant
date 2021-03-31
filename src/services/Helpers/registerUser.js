87206ad71dd19dc7512e56e046b1efc309064679
Krum Kuzmov <kkuzmov@gmail.com>
2021-03-31 10:54:15 +0300

    add email regex validation to register

--- src/services/Helpers/registerUser.js
+++ src/services/Helpers/registerUser.js
@@ -1,5 +1,9 @@
 function testRegisterUser(input){
-    if(input.email.length < 6){
+    let pattern = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+
+    if(!pattern.test(input.email)){
+        return 'Please provide a correct email to sign-up - REGEX TEST!'
+    }else if(input.email.length < 6){
         return 'Please provide a correct email to sign-up'
     }else if(!input.email.includes('@')){
         return 'Please provide a correct email to sign-up - NO @!'
