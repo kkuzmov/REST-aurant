0dbbeaafad2e9c3ac639274813a6daaba340a90d
Krum Kuzmov <kkuzmov@gmail.com>
2021-04-02 15:25:07 +0300

    add Auth route

--- src/components/Auth/Auth.js
+++ src/components/Auth/Auth.js
@@ -1,3 +1,20 @@
 import React, {useEffect, useState} from 'react';
-import firebaseApp from '../../firebase/firebase.config';
+import { firebaseApp } from '../../firebase/firebase.config';
 
+export const AuthContext = React.createContext();
+
+export const AuthProvider = ({ children }) => {
+const [currentUser, setCurrentUser] = useState(null);
+
+useEffect(()=>{
+firebaseApp.auth().onAuthStateChanged(setCurrentUser);
+}, [])
+
+return(
+    <AuthContext.Provider
+        value={{currentUser}}
+    >
+        {children}
+    </AuthContext.Provider>
+)
+}
