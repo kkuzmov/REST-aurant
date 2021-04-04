247e6abea92d39a7c9456d57b8e169641fe69ec0
Krum Kuzmov <kkuzmov@gmail.com>
2021-04-04 17:24:44 +0300

    fix homepage guest

--- src/components/HomepageGuest/HomepageGuest.js
+++ src/components/HomepageGuest/HomepageGuest.js
@@ -14,17 +14,17 @@ class HomepageGuest extends Component{
             <article className="restaurant-card card-left-guest">
                 <img src="https://www.mychoice.bg/wp-content/uploads/2020/05/959x578-restorant-1-1.jpg" alt="simeonovo-restaurant"/>
                 <h4>Simeonovo Restaurant</h4>
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Totam, autem.</p>
+                <p>"...Lorem ipsum dolor sit amet, consectetur adipisicing elit. Totam, autem..."</p>
             </article>
             <article className="restaurant-card card-middle-guest">
                 <img src="https://socialcafe.bg/pics/restaurant/2.jpg" alt="boyana-restaurant"/>
-                <h4>Boyana Restaurant</h4>
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Totam, autem.</p>
+                <h4>Lozenets Restaurant</h4>
+                <p>"...Lorem ipsum dolor sit amet, consectetur adipisicing elit. Totam, autem..."</p>
             </article>
             <article className="restaurant-card card-right-guest">
                 <img src="https://rezzo.bg/files/images/14/fit_314_228.jpg" alt="bankya-restaurant"/>
-                <h4>Bankya Restaurant</h4>
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Totam, autem.</p>
+                <h4>Dragalevtsi Restaurant</h4>
+                <p>"...Lorem ipsum dolor sit amet, consectetur adipisicing elit. Totam, autem..."</p>
             </article>       
         </article>
         <article className="login-or-signup">
