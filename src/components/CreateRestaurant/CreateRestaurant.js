3e33ff0d02eda8e111a7cc091bf78d9d93aced99
Krum Kuzmov <kkuzmov@gmail.com>
2021-04-07 18:43:25 +0300

    add category to restaurant create route

--- src/components/CreateRestaurant/CreateRestaurant.js
+++ src/components/CreateRestaurant/CreateRestaurant.js
@@ -25,6 +25,7 @@ function CreateRestaurant(){
             'rating': Number(event.target.rating.value),
             'description': event.target.description.value,
             'imageUrl': event.target.imageUrl.value,
+            'category': event.target.category.value,
             'ratedBy': 0,
             'likedBy': [],
             'creator': currentUser.uid
@@ -65,12 +66,24 @@ function CreateRestaurant(){
                     <article className="form-input">
                         <input type="text" name="location" placeholder="City" required />
                     </article>
-
                     <article className="form-input">
-                        <input type="number" min="1" max="5" step="0.5" name="rating" placeholder="Rating" required />
+                        {/* <input type="select" min="1" max="5" step="0.5" name="rating" placeholder="Rating" required /> */}
+                        <select name="category">
+                            <option value="" disabled selected>Select your option</option>
+                            <option value="Ethnic">Ethnic</option>
+                            <option value="Fast food">Fast food</option>
+                            <option value="Fast casual">Fast casual</option>
+                            <option value="Casual dining">Casual dining</option>
+                            <option value="Premium casual">Premium casual</option>
+                            <option value="Family style">Family style</option>
+                            <option value="Fine Dining">Fine Dining</option>
+                        </select>
                     </article>
                     <article className="form-input textarea-input">
                         <textarea name="description" id="description" cols="34" rows="10" placeholder="Write a review here..."></textarea>
+                    </article>
+                    <article className="form-input">
+                        <input type="number" min="1" max="5" step="0.5" name="rating" placeholder="Rating" required />
                     </article>
                     <article className="form-input">
                         {/* <input type="file" id="photo" name="uploaded-picture" /> */}
