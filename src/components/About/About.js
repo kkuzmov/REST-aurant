0d72f8f3e1f53caf3a83c755c20ea1b740ef5a98
Krum Kuzmov <kkuzmov@gmail.com>
2021-04-09 00:28:41 +0300

    add about route styling and information about app

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -14,17 +14,19 @@ function About(){
 
     useEffect(()=>{
         getAllRestaurants()
-        .then(res => setRestaurantsCount(res.docs.length))
+        .then(res => setRestaurantsCount(res.docs.length - 1))
     }, [])
     
 
     return(
-        <section className="about-page">
+        <>
         <h1 className="page-heading">About us</h1>
-        <p></p>
-        <h3>Currently we have {restaurantsCount} restaurant ratings in our database!</h3>
-        <p><Link to="/login">Join us</Link> and create your own ratings and reviews!</p>
+        <section className="about-page">
+        <p className="about-info">&copy; REST-aurant is a project, created for foodies by a foodie myself. &copy; REST-aurant uses REST principles to deliver up-to-date information and recomendations about all kinds of food places everywhere in the foodie world. Enjoy existing reviews and create your own. Expect future improvements and application expansions as users grow!</p>
+        <h2>Currently we have more than {restaurantsCount} restaurant reviews in our database!</h2>
+        <h2><Link to="/register" className="join-us">Join us</Link> and create your own ratings and reviews!</h2>
         </section>
+        </>
     )
     
 }
