fd3738adf3ac5661720398345b984f28ecce8fe0
Krum Kuzmov <kkuzmov@gmail.com>
2021-04-08 17:32:25 +0300

    add pros and cons to details route

--- src/components/Details/Details.js
+++ src/components/Details/Details.js
@@ -55,6 +55,16 @@ function DetailsWithFunction({match}){
                 <h1 className="restaurant-details-name">{restaurant.location}</h1>
                 <h3 className="rated-by">Liked by {restaurant.ratedBy} people</h3>
                 <p className="restaurant-details-description">{restaurant.description}</p>
+                <section className="pros-and-cons-details">
+                    <section className="heading-and-text-pros-cons">
+                        <h3>Pros</h3>
+                        <p className="pros">{restaurant.pros}</p>
+                    </section>
+                    <section className="heading-and-text-pros-cons">
+                        <h3>Cons</h3>
+                        <p className="cons">{restaurant.cons}</p>
+                    </section>
+                </section>
                 {/* <p><img src={map} alt="map" className="google-api-sample-pic" /></p> */}
                 <article className="details-buttons">
                     <button className="site-button"onClick={deleteRestaurant}>Delete</button>
