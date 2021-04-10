11e422aeceea0a5736f20a91a6bb2437bf6d311a
Krum Kuzmov <kkuzmov@gmail.com>
2021-04-10 15:55:19 +0300

    add search functionality in liked restaurants

--- src/components/LikedRestaurants/LikedRestaurants.js
+++ src/components/LikedRestaurants/LikedRestaurants.js
@@ -12,6 +12,7 @@ function LikedRestaurants(){
     const [ errMessage, setErrMessage ] = useState('');
     const { currentUser } = useContext(AuthContext);
     let [restaurants, setAllRestaurants] = useState([]);
+    let [currentRestaurantsToRender, setCurrentRestaurantsToRender] = useState([]);
     let allLikedRestaurants;
     
     useEffect(()=>{
@@ -23,13 +24,26 @@ function LikedRestaurants(){
             .get()
             .then(res => {
             let allRestaurants = res.docs.map(restaurant => restaurant = {...restaurant.data(), id: restaurant.id})
-            setAllRestaurants(allRestaurants)
+            setAllRestaurants(allRestaurants);
+            setCurrentRestaurantsToRender(allRestaurants);
         })
         .catch(err => setErrMessage(err.message))
     }
     }, [])
+
+    function performSearch(event){
+        event.preventDefault()
+        let filteredResults = restaurants.filter(rest => 
+            rest.name.toLowerCase().includes(event.target.search.value.toLowerCase())||
+            rest.location.toLowerCase().includes(event.target.search.value.toLowerCase())
+        )
+        setCurrentRestaurantsToRender(filteredResults)
+    }
+
+   // used only after restaurants have been set
+
     if(restaurants.length > 0){
-        allLikedRestaurants = restaurants.map(x => <Restaurant 
+        allLikedRestaurants = currentRestaurantsToRender.map(x => <Restaurant 
             key={x.id}
             imageUrl={x.imageUrl}
             name={x.name}
@@ -42,10 +56,16 @@ function LikedRestaurants(){
     }else{
         allLikedRestaurants = <h1>You didn't like a single review yet...</h1>
     }
+    
 
     return(
         <>
         <h1 className="page-heading">Restaurants liked</h1>
+        <form onSubmit={performSearch}>
+        <label htmlFor="search">Search here</label>
+        <input type="search" name="search"></input>
+        <input type="submit" value="Search"></input>
+        </form>
         <article className="all-rated-restaurants">
                 {allLikedRestaurants}
         </article>
