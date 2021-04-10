11074c59907899825cff90af37b6f9728f1bfcc7
Krum Kuzmov <kkuzmov@gmail.com>
2021-04-10 16:22:28 +0300

    add search functionality in all restaurants - class component

--- src/components/RestaurantsWrapper/RestaurantsWrapper.js
+++ src/components/RestaurantsWrapper/RestaurantsWrapper.js
@@ -9,9 +9,11 @@ class RestaurantsWrapper extends Component{
     constructor(props){
         super(props)
         this.state = {
-            'restaurants': []
+            'restaurants': [],
+            'currentRestaurants': []
         }
         this.getRestaurants = this.getRestaurants.bind(this);
+        this.performSearch = this.performSearch.bind(this);
     }
     getRestaurants(){
         db.collection('restaurants')
@@ -22,14 +24,24 @@ class RestaurantsWrapper extends Component{
                 allFetched.push({...restaurant.data(), id: restaurant.id})
             })
             this.setState({'restaurants': allFetched})
+            this.setState({'currentRestaurants': allFetched})
         })
         .catch(err => console.log(err))
     }
     componentDidMount(){
         this.getRestaurants();
     }
+    performSearch(event){
+        event.preventDefault();
+        let filteredResults = this.state.restaurants.filter(rest => 
+            rest.name.toLowerCase().includes(event.target.search.value.toLowerCase())||
+            rest.location.toLowerCase().includes(event.target.search.value.toLowerCase())||
+            rest.description.toLowerCase().includes(event.target.search.value.toLowerCase())
+        )
+        this.setState({'currentRestaurants': filteredResults})
+    }
     render(){
-        let allRestaurants = this.state.restaurants.map(x => x = <Restaurant 
+        let allRestaurants = this.state.currentRestaurants.map(x => x = <Restaurant 
             key={x.id}
             imageUrl={x.imageUrl}
             name={x.name}
@@ -41,6 +53,11 @@ class RestaurantsWrapper extends Component{
         return (
             <>
             <h1 className="page-heading">All restaurants</h1>
+            <form onSubmit={this.performSearch}>
+            <label htmlFor="search">Search here</label>
+            <input type="search" name="search"></input>
+            <input type="submit" value="Search" className='site-button'></input>
+            </form>
             <article className="all-rated-restaurants">
                 {allRestaurants}
             </article>
