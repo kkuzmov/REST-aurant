c9cf44eb32d430113a39fc1bb403cf5d01a5d1b0
Krum Kuzmov <kkuzmov@gmail.com>
2021-04-17 19:34:36 +0300

    fix navigation responsiveness

--- src/components/Header/Navigation/NavigationGuest.js
+++ src/components/Header/Navigation/NavigationGuest.js
@@ -7,7 +7,7 @@ import { style } from './Navigation.css';
 class NavigationGuest extends Component{
     render(){
         return (
-            <ul className="navigation">
+            <ul className="navigation-guest">
                 <Link to="/login" className="nav-link">Login</Link>
                 <Link to="/register" className="nav-link">Register</Link>
                 <Link to="/about" className="nav-link">About</Link>
