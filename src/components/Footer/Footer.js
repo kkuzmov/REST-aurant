d19e61bea7d3386d56b6df07e0be035576da5386
Krum Kuzmov <kkuzmov@gmail.com>
2021-04-23 00:29:22 +0300

    fix footer link to linkedIn

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom';
 
 function Footer(){
     return(
-        <footer className="main-footer">&copy; 2021, created by <Link to="https://www.linkedin.com/in/krumkuzmov/" className="footer-link" ><a>Krum Kuzmov</a></Link></footer>
+        <footer className="main-footer">&copy; 2021, created by <a href="https://www.linkedin.com/in/krumkuzmov/" className="footer-link" >Krum Kuzmov</a></footer>
     )
 }
 
