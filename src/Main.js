cc2005b1ac3a3bec65d6605064f5ea751b7e7022
Krum Kuzmov <kkuzmov@gmail.com>
2021-06-10 21:59:22 +0300

    a few formattings

--- src/Main.js
+++ src/Main.js
@@ -1,56 +1,51 @@
-import React, {Component} from 'react';
-import { Route, Switch } from 'react-router-dom';
+import React, { Component } from "react";
+import { Route, Switch } from "react-router-dom";
 
-import Header from './components/Header/Header';
-import Footer from './components/Footer/Footer';
-import HomepageContainer from './components/HomepageContainer/HomepageContainer';
-import Login from './components/Login/Login';
-import Register from './components/Register/Register';
-import CreateRestaurant from './components/CreateRestaurant/CreateRestaurant';
-import Details from './components/Details/Details';
-import EditRestaurant from './components/EditRestaurant/EditRestaurant';
-import AllRestaurants from './components/AllRestaurants/AllRestaurants';
-import RatedRestaurants from './components/RatedRestaurants/RatedRestaurants';
-import LikedRestaurants from './components/LikedRestaurants/LikedRestaurants';
-import Profile from './components/Profile/Profile';
-import ErrorPage from './components/ErrorPage/ErrorPage';
-import About from './components/About/About';
-import TEST from './components/TEST/TEST';
-import { AuthProvider } from './components/Auth/Auth';
+import Header from "./components/Header/Header";
+import Footer from "./components/Footer/Footer";
+import HomepageContainer from "./components/HomepageContainer/HomepageContainer";
+import Login from "./components/Login/Login";
+import Register from "./components/Register/Register";
+import CreateRestaurant from "./components/CreateRestaurant/CreateRestaurant";
+import Details from "./components/Details/Details";
+import EditRestaurant from "./components/EditRestaurant/EditRestaurant";
+import AllRestaurants from "./components/AllRestaurants/AllRestaurants";
+import RatedRestaurants from "./components/RatedRestaurants/RatedRestaurants";
+import LikedRestaurants from "./components/LikedRestaurants/LikedRestaurants";
+import Profile from "./components/Profile/Profile";
+import ErrorPage from "./components/ErrorPage/ErrorPage";
+import About from "./components/About/About";
+import TEST from "./components/TEST/TEST";
+import { AuthProvider } from "./components/Auth/Auth";
 
+import { style } from "./Main.css";
 
-
-import { style } from './Main.css';
-
-
-
-class Main extends Component{
-    render(){
-        return(
-            <AuthProvider>
-            <div className="container">
-            <Header />
-            <Switch>
-            <Route path="/" exact component={HomepageContainer}/>
-            <Route path="/create" exact component={CreateRestaurant}/>
-            <Route path="/all-restaurants" component={AllRestaurants}/>
-            <Route path="/:id/rated-restaurants" component={RatedRestaurants}/>
-            <Route path="/:id/liked-restaurants" component={LikedRestaurants}/>
-            <Route path="/profile" component={Profile}/>
-            <Route path="/login" component={Login}/>
-            <Route path="/register" component={Register}/>
-            <Route path="/about" component={About}/>
-            <Route path="/test" component={TEST}/>
-            <Route path="/details/:id" component={Details}/>
-            <Route path="/edit/:id" component={EditRestaurant}/>
-            <Route component={ErrorPage}/>
-            </Switch>
-            <Footer />
-            </div>
-            </AuthProvider>
-        )
-    }
+class Main extends Component {
+  render() {
+    return (
+      <AuthProvider>
+        <div className="container">
+          <Header />
+          <Switch>
+            <Route path="/" exact component={HomepageContainer} />
+            <Route path="/create" exact component={CreateRestaurant} />
+            <Route path="/all-restaurants" component={AllRestaurants} />
+            <Route path="/:id/rated-restaurants" component={RatedRestaurants} />
+            <Route path="/:id/liked-restaurants" component={LikedRestaurants} />
+            <Route path="/profile" component={Profile} />
+            <Route path="/login" component={Login} />
+            <Route path="/register" component={Register} />
+            <Route path="/about" component={About} />
+            <Route path="/test" component={TEST} />
+            <Route path="/details/:id" component={Details} />
+            <Route path="/edit/:id" component={EditRestaurant} />
+            <Route component={ErrorPage} />
+          </Switch>
+          <Footer />
+        </div>
+      </AuthProvider>
+    );
+  }
 }
-
 
 export default Main;
--- src/components/Auth/Auth.js
+++ src/components/Auth/Auth.js
@@ -1,20 +1,18 @@
-import React, {useEffect, useState} from 'react';
-import { firebaseApp } from '../../firebase/firebase.config';
+import React, { useEffect, useState } from "react";
+import { firebaseApp } from "../../firebase/firebase.config";
 
 export const AuthContext = React.createContext();
 
 export const AuthProvider = ({ children }) => {
-const [currentUser, setCurrentUser] = useState(null);
+  const [currentUser, setCurrentUser] = useState(null);
 
-useEffect(()=>{
-firebaseApp.auth().onAuthStateChanged(setCurrentUser);
-}, [])
+  useEffect(() => {
+    firebaseApp.auth().onAuthStateChanged(setCurrentUser);
+  }, []);
 
-return(
-    <AuthContext.Provider
-        value={{currentUser}}
-    >
-        {children}
+  return (
+    <AuthContext.Provider value={{ currentUser }}>
+      {children}
     </AuthContext.Provider>
-)
-}
+  );
+};
