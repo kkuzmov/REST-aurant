b583005b4394b8266f6a8cff5fb80d98add3bb6a
Krum Kuzmov <kkuzmov@gmail.com>
2021-06-26 15:44:13 +0300

    fix search in liked restaurants

--- src/components/LikedRestaurants/LikedRestaurants.js
+++ src/components/LikedRestaurants/LikedRestaurants.js
@@ -1,77 +1,79 @@
-import { useContext, useEffect, useState } from 'react';
-import {  useHistory } from 'react-router-dom';
-import { AuthContext } from '../Auth/Auth';
-import { db } from '../../firebase/firebase.config';
-import RestaurantInAllRestaurants from '../RestaurantInAllRestaurants/RestaurantInAllRestaurants';
-import ErrorMessage from '../ErrorMessage/ErrorMessage';
+import { useContext, useEffect, useState } from "react";
+import { useHistory } from "react-router-dom";
+import { Ellipsis } from "react-spinners-css";
 
+import { AuthContext } from "../Auth/Auth";
+import { db } from "../../firebase/firebase.config";
+import RestaurantInAllRestaurants from "../RestaurantInAllRestaurants/RestaurantInAllRestaurants";
+import ErrorMessage from "../ErrorMessage/ErrorMessage";
 
+function LikedRestaurants() {
+  let history = useHistory();
+  const [errMessage, setErrMessage] = useState("");
+  let [restaurants, setAllRestaurants] = useState([]);
+  let [currentRestaurants, setCurrentRestaurants] = useState([]);
+  const { currentUser } = useContext(AuthContext);
+  let allLikedRestaurants;
 
-function LikedRestaurants(){
-    let history = useHistory();
-    const [ errMessage, setErrMessage ] = useState('');
-    const { currentUser } = useContext(AuthContext);
-    let [restaurants, setAllRestaurants] = useState([]);
-    let [currentRestaurantsToRender, setCurrentRestaurantsToRender] = useState([]);
-    let allLikedRestaurants;
-    
-    useEffect(()=>{
-        if(currentUser == null){
-            history.push('/login')
-        }else{
-        db.collection('restaurants')
-            .where('likedBy', 'array-contains', currentUser.uid)
-            .get()
-            .then(res => {
-            let allRestaurants = res.docs.map(restaurant => restaurant = {...restaurant.data(), id: restaurant.id})
-            setAllRestaurants(allRestaurants);
-            setCurrentRestaurantsToRender(allRestaurants);
+  useEffect(() => {
+    if (currentUser == null) {
+      history.push("/login");
+    } else {
+      db.collection("restaurants")
+        .where("likedBy", "array-contains", currentUser.uid)
+        .get()
+        .then((res) => {
+          let allRestaurants = res.docs.map(
+            (restaurant) =>
+              (restaurant = { ...restaurant.data(), id: restaurant.id })
+          );
+          setAllRestaurants(allRestaurants);
+          setCurrentRestaurants(allRestaurants);
         })
-        .catch(err => setErrMessage(err.message))
+        .catch((err) => setErrMessage(err.message));
     }
-    }, [])
+  }, []);
 
-    function performSearch(event){
-        event.preventDefault()
-        let filteredResults = restaurants.filter(rest => 
-            rest.name.toLowerCase().includes(event.target.search.value.toLowerCase())||
-            rest.location.toLowerCase().includes(event.target.search.value.toLowerCase())||
-            rest.description.toLowerCase().includes(event.target.search.value.toLowerCase())
-        )
-        setCurrentRestaurantsToRender(filteredResults)
-    }
+  function performSearch(event) {
+    event.preventDefault();
+    let query = event.target.value.toLowerCase();
 
-   // used only after restaurants have been set
+    let filteredResults = restaurants.filter(
+      (rest) =>
+        rest.name.toLowerCase().includes(query.toLowerCase()) ||
+        rest.location.toLowerCase().includes(query.toLowerCase()) ||
+        rest.description.toLowerCase().includes(query.toLowerCase()) ||
+        rest.category.toLowerCase().includes(query)
+    );
+    setCurrentRestaurants(filteredResults);
+  }
 
-    if(currentRestaurantsToRender.length > 0){
-        allLikedRestaurants = currentRestaurantsToRender.map(x => <RestaurantInAllRestaurants 
-            key={x.id}
-            imageUrl={x.imageUrl}
-            name={x.name}
-            description={x.description}
-            location={x.location}
-            rating={x.rating}
-            category={x.category}
-            id={x.id}/>
-        )
-    }else{
-        allLikedRestaurants = <h1>You didn't like a single review yet...</h1>
-    }
-    
-
-    return(
-        <>
-        <h1 className="page-heading">Restaurants reviews you liked</h1>
-        <form onSubmit={performSearch} className="search-form">
-            <label htmlFor="search" className="search-label">Search in restaurants</label>
-            <input type="search" name="search" className="search-input"></input>
-            <input type="submit" value="Find" className='site-button'></input>
-        </form>
-        <article className="all-rated-restaurants">
-                {allLikedRestaurants}
-        </article>
-        <ErrorMessage>{errMessage}</ErrorMessage>
-        </>
-    )
+  return (
+    <>
+      <h1 className="page-heading">Restaurants reviews you liked</h1>
+      <form className="search-form">
+        <label htmlFor="search" className="search-label">
+          Search by restaurant name, location or description
+        </label>
+        <input
+          type="search"
+          name="search"
+          className="search-input"
+          onChange={performSearch}
+        ></input>
+        <input type="submit" value="Find" className="site-button"></input>
+      </form>
+      <article className="all-rated-restaurants">
+        {currentRestaurants.length == 0 ? (
+          <Ellipsis color="#513C2C" size={100} />
+        ) : (
+          currentRestaurants.map((x) => (
+            <RestaurantInAllRestaurants restaurant={x} key={x.id} />
+          ))
+        )}
+      </article>
+      <ErrorMessage>{errMessage}</ErrorMessage>
+    </>
+  );
 }
 export default LikedRestaurants;
